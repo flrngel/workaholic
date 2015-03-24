0f726b72e052fa4567bc05b7771f6407f2eae03b
flrngel <flrngel@gmail.com>
2015-03-24 09:28:08 +0900

    dir change

---
+++ boss.js
@@ -0,0 +1,21 @@
+var cfg=require("config.json");
+var forever=require("forever-monitor");
+
+var msg={
+  "error": function(){
+  }
+};
+
+for(var i=0;i<cfg.worker.number;i++){
+  var child_worker=new (forever.Monitor)('worker/worker.js',{
+    silent: true,
+  });
+
+  child_worker.on('exit',msg.error);
+
+  child_worker.start();
+}
+
+var child_front=new (forever.Monitor)('worker/worker.js',{
+  silent: true,
+});
